a184e7ab9d02c1a9dfff284746e6a759542317c8
Manasi Kshirsagar <manasikshirsagar@Manasis-MacBook-Air.local>
2020-04-22 23:12:10 +0530

    String Operations

--- Variables.py
+++ Variables.py
@@ -3,3 +3,39 @@ print(message)
 
 message=("Python is great language to learn")
 print(message)
+
+
+###String Concatenation
+name="Manasi"
+print("\nHello "+ name +",would you like to learn some Python today?\n")
+
+###Uppercase lowercase TitleCase
+firstname="manasi kshirsagar"
+print(firstname.title())
+print(firstname.lower())
+print(firstname.upper())
+
+### String with quotes
+quote=('\nAlbert Einstine once said, "A person who never made mistake never tried anything new"')
+print(quote)
+
+famous_person="Albert Einstine "
+quote1=(famous_person+'once said, "A person who never made mistake never tried anything new"')
+print(quote1)
+
+#### StrippingNames
+myname=" Manasi Kshirsagar "
+print(myname.strip())
+print(myname.lstrip())
+print(myname.rstrip())
+
+myname="\tPython is my favourite language  "
+print(myname.strip())
+print(myname.lstrip())
+print(myname.rstrip()) ## only consider \t for the rstrip function
+
+
+myname="\tPython is my easy language to learn  \n"
+print(myname.strip())
+print(myname.lstrip()) ## only consider \n for the lstrip function
+print(myname.rstrip()) ## only consider \t for the rstrip function
